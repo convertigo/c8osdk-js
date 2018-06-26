27a09e2508f7f7304a513cc97f48d033bca353dd
Grimont <grimont.c@gmail.com>
2018-06-26 14:37:35 +0200

    integrate sl

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -55,14 +55,45 @@ module.exports = function(config) {
         autoWatch: false,
 
         browsers: [
-            'Chrome'
+            'Chrome', 'Firefox', 'IE'
         ],
 
         customLaunchers: {
             ChromeTravisCi: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
-            }
+            },
+            sl_chrome: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'Windows 7',
+                version: '35'
+              },
+              sl_firefox: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                version: '30'
+              },
+              sl_ios_safari: {
+                base: 'SauceLabs',
+                browserName: 'iphone',
+                platform: 'OS X 10.9',
+                version: '7.1'
+              },
+              sl_ie_11: {
+                base: 'SauceLabs',
+                browserName: 'internet explorer',
+                platform: 'Windows 8.1',
+                version: '11'
+              },
+              sl_android: {
+                base: 'SauceLabs',
+                browserName: 'Browser',
+                platform: 'Android',
+                version: '4.4',
+                deviceName: 'Samsung Galaxy S3 Emulator',
+                deviceOrientation: 'portrait'
+              }
         },
 
         singleRun: true
@@ -70,8 +101,10 @@ module.exports = function(config) {
 
     if (process.env.TRAVIS){
         configuration.browsers = [
-            'ChromeTravisCi'
+            Object.keys(customLaunchers)
         ];
+        configuration.reporters = ['dots', 'saucelabs'];
+        configuration.singleRun = true;
     }
 
     config.set(configuration);
