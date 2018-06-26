a1aed33a98afe1d77e66ae5e73ce14e88a2d0c83
Grimont <grimont.c@gmail.com>
2018-06-26 15:32:04 +0200

    update config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -59,36 +59,10 @@ module.exports = function(config) {
         ],
 
         customLaunchers: {
-            sl_chrome: {
-                base: 'SauceLabs',
-                browserName: 'chrome',
-                platform: 'Windows 7',
-                version: '35'
-              },
               sl_firefox: {
                 base: 'SauceLabs',
                 browserName: 'firefox',
                 version: '30'
-              },
-              sl_ios_safari: {
-                base: 'SauceLabs',
-                browserName: 'iphone',
-                platform: 'OS X 10.9',
-                version: '7.1'
-              },
-              sl_ie_11: {
-                base: 'SauceLabs',
-                browserName: 'internet explorer',
-                platform: 'Windows 8.1',
-                version: '11'
-              },
-              sl_android: {
-                base: 'SauceLabs',
-                browserName: 'Browser',
-                platform: 'Android',
-                version: '4.4',
-                deviceName: 'Samsung Galaxy S3 Emulator',
-                deviceOrientation: 'portrait'
               }
         },
 
